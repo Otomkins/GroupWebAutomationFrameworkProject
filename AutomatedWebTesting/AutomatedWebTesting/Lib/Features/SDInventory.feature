ae9c2890a39801c80bc23754ab3b2232cbcee3f1
Jacob Stoneman (stonemaj) <jacob679@hotmail.co.uk>
2021-02-10 16:14:55 +0000

    SDInventory and tests

---
+++ AutomatedWebTesting/AutomatedWebTesting/Lib/Features/SDInventory.feature
@@ -0,0 +1,14 @@
+﻿Feature: SDInventory
+	Simple calculator for adding two numbers
+
+@Inventory
+Scenario: Add item to cart
+	Given I am on the inventory page
+	When I click on the first products add to cart button
+	Then the cart label is equal to 1
+
+@Inventory
+Scenario: Add item to cart changes button to remove
+	Given I am on the inventory page
+	When I click on the first products add to cart button
+	Then the first products add to cart button text is REMOVE
---
+++ AutomatedWebTesting/AutomatedWebTesting/Lib/Steps/SDInventorySteps.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using TechTalk.SpecFlow;
+
+namespace AutomatedWebTesting
+{
+    [Binding]
+    public class SDInventorySteps
+    {
+        SDWebsite<ChromeDriver> SDWebsite = new SDWebsite<ChromeDriver>();
+
+        [Given(@"I am on the inventory page")]
+        public void GivenIAmOnTheInventoryPage()
+        {
+            SDWebsite.SDInventory.GoToInventory();
+        }
+        
+        [When(@"I click on the first products add to cart button")]
+        public void WhenIClickOnTheFirstProductsAddToCartButton()
+        {
+            SDWebsite.SDInventory.AddProductToCart(0);
+        }
+        
+        [Then(@"the cart label is equal to (.*)")]
+        public void ThenTheCartLabelIsEqualTo(int p0)
+        {
+            Assert.That(SDWebsite.SDInventory.GetCartCount(), Is.EqualTo(1));
+        }
+
+        [Then(@"the first products add to cart button text is REMOVE")]
+        public void ThenTheFirstProductsAddToCartButtonTextIsREMOVE()
+        {
+            Assert.That(SDWebsite.SDInventory.GetButtonState(0), Is.EqualTo("REMOVE"));
+        }
+
+        [After]
+        public void Cleanup() => SDWebsite.Dispose();
+    }
+}
