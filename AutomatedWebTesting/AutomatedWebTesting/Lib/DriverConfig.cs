8254b12b6481cd29afaf2a34f1d1af0f813a514f
Daniel Layland <laylandwork@gmail.com>
2021-02-10 14:41:59 +0000

    added basic setup for driver config and website

--- AutomatedWebTesting/AutomatedWebTesting/Lib/DriverConfig.cs
+++ AutomatedWebTesting/AutomatedWebTesting/Lib/DriverConfig.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,25 @@ using System.Threading.Tasks;
 
 namespace AutomatedWebTesting.Lib
 {
-	class DriverConfig
-	{
-	}
+    public class SeleniumDriverConfig<T> where T : IWebDriver, new()
+    {
+        public T Driver { get; set; }
+
+        public SeleniumDriverConfig(int pageLoadInSecs, int implicitWaitInSecs)
+        {
+            Driver = new T();
+            DriverSetup(pageLoadInSecs, implicitWaitInSecs);
+        }
+
+        private void DriverSetup(int pageLoadInSecs, int implicitWaitInSecs)
+        {
+            SetDriverConfiguration(pageLoadInSecs, implicitWaitInSecs);
+        }
+
+        private void SetDriverConfiguration(int pageLoadInSecs, int implicitWaitInSecs)
+        {
+            Driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(implicitWaitInSecs);
+            Driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(pageLoadInSecs);
+        }
+    }
 }
--- AutomatedWebTesting/AutomatedWebTesting/Lib/Pages/SDWebsite.cs
+++ AutomatedWebTesting/AutomatedWebTesting/Lib/Pages/SDWebsite.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,18 @@ using System.Threading.Tasks;
 
 namespace AutomatedWebTesting.Lib.Pages
 {
-	class SDWebsite
-	{
-	}
+    public class SDWebsite<driver> : IDisposable where driver : IWebDriver, new()
+    {
+        public driver SeleniumDriver { get; internal set; }
+
+        public SDWebsite(int pageLoadInSecs = 10, int implicitWaitInSecs = 10)
+        {
+            SeleniumDriver = new SeleniumDriverConfig<driver>(pageLoadInSecs, implicitWaitInSecs).Driver;
+        }
+
+        public void Dispose()
+        {
+            SeleniumDriver.Quit();
+        }
+    }
 }
