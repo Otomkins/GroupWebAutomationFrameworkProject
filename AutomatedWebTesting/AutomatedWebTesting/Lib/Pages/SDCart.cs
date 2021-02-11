4f6316f7c4d2946972c92cade8ad78525987071a
Zair <michaelxzr@gmail.com>
2021-02-11 18:20:06 +0000

    Update SDCart.cs

--- AutomatedWebTesting/AutomatedWebTesting/Lib/Pages/SDCart.cs
+++ AutomatedWebTesting/AutomatedWebTesting/Lib/Pages/SDCart.cs
@@ -1,48 +1,55 @@
-﻿using OpenQA.Selenium;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using OpenQA.Selenium;
 
 namespace AutomatedWebTesting
 {
     public class SDCart
     {
-		private IWebDriver _driver;
-		private string _inventoryPageURL = "https://www.saucedemo.com/inventory.html";
+		private readonly string _inventoryPageURL = "https://www.saucedemo.com/inventory.html";
+		private readonly IWebDriver _driver;
 
-		public SDCart(IWebDriver driver)
-		{
-			_driver = driver;
-		}
+		public SDCart(IWebDriver driver) => _driver = driver;
 
-		private IWebElement cart => _driver.FindElement(By.Id("shopping_cart_container"));
-		private IReadOnlyCollection<IWebElement> inventoryItemsAddToCart => _driver.FindElements(By.ClassName("btn_inventory"));
-		private IReadOnlyCollection<IWebElement> inventoryItemRemoveButtons => _driver.FindElements(By.ClassName("cart_button"));
-		private IWebElement redShirtRemoveButton => _driver.FindElement(By.ClassName("cart_button"));
+		private IReadOnlyCollection<IWebElement> InventoryItemsAddToCart => _driver.FindElements(By.ClassName("btn_inventory"));
 
-		private IWebElement cartItemCount => _driver.FindElement(By.ClassName("shopping_cart_badge"));
+		private IReadOnlyCollection<IWebElement> InventoryItemRemoveButtons => _driver.FindElements(By.ClassName("cart_button"));
 
-		private IWebElement backpack => _driver.FindElement(By.Id("item_4_title_link"));
-		private IWebElement bikeLight => _driver.FindElement(By.Id("item_0_title_link"));
-		private IWebElement boltShirt => _driver.FindElement(By.Id("item_1_title_link"));
-		private IWebElement fleece => _driver.FindElement(By.Id("item_5_title_link"));
-		private IWebElement onesie => _driver.FindElement(By.Id("item_2_title_link"));
-        private IWebElement redShirt => _driver.FindElement(By.Id("item_3_title_link"));
+		private IWebElement Cart => _driver.FindElement(By.Id("shopping_cart_container"));
+
+		private IWebElement RedShirtRemoveButton => _driver.FindElement(By.ClassName("cart_button"));
+
+		private IWebElement CartItemCount => _driver.FindElement(By.ClassName("shopping_cart_badge"));
+
+		private IWebElement Backpack => _driver.FindElement(By.Id("item_4_title_link"));
+
+		private IWebElement BikeLight => _driver.FindElement(By.Id("item_0_title_link"));
+
+		private IWebElement BoltShirt => _driver.FindElement(By.Id("item_1_title_link"));
+
+		private IWebElement Fleece => _driver.FindElement(By.Id("item_5_title_link"));
+
+		private IWebElement Onesie => _driver.FindElement(By.Id("item_2_title_link"));
+
+        private IWebElement RedShirt => _driver.FindElement(By.Id("item_3_title_link"));
 
         public void AddAllProductsToCart()
 		{
-            foreach (IWebElement item in inventoryItemsAddToCart.ToArray())
+            foreach (IWebElement item in InventoryItemsAddToCart.ToArray())
             {
 				item.Click();
             }
 		}
 
 		public void GoToInventory() => _driver.Navigate().GoToUrl(_inventoryPageURL);
-		public void GoToCheckout() => cart.Click();
-		public void RemoveItemFromCart(int num) => inventoryItemRemoveButtons.ToArray()[num].Click();
-		public void RemoveRedShirtFromCart() => redShirtRemoveButton.Click();
+
+		public void GoToCheckout() => Cart.Click();
+
+		public void RemoveItemFromCart(int num) => InventoryItemRemoveButtons.ToArray()[num].Click();
+
+		public void RemoveRedShirtFromCart() => RedShirtRemoveButton.Click();
+
 		public bool CheckIfItemIsRemovedFromCart(string itemName)
         {
 			_driver.Navigate().Refresh();
@@ -70,29 +77,29 @@ namespace AutomatedWebTesting
 			return false;
 		}
 
-        public int GetCartCount() => Convert.ToInt32(cartItemCount.Text.ToString());
+        public int GetCartCount() => Convert.ToInt32(CartItemCount.Text.ToString());
 
 		internal void ClickItemNameCartLink(string itemName)
 		{
 			switch (itemName)
 			{
 				case "backpack":
-					backpack.Click();
+					Backpack.Click();
 					break;
 				case "bikeLight":
-					bikeLight.Click();
+					BikeLight.Click();
 					break;
 				case "boltShirt":
-					boltShirt.Click();
+					BoltShirt.Click();
 					break;
 				case "fleece":
-					fleece.Click();
+					Fleece.Click();
 					break;
 				case "onesie":
-					onesie.Click();
+					Onesie.Click();
 					break;
 				case "redShirt":
-					redShirt.Click();
+					RedShirt.Click();
 					break;
 			}
 		}
