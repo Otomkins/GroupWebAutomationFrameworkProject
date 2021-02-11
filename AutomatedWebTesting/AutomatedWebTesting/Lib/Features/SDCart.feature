ea813f617b522a589eac1c1b54f71a0ccbf6345a
Oliver Tomkins <otomkins@yahoo.co.uk>
2021-02-11 17:11:11 +0000

    Added SD Cart Features & Tests

---
+++ AutomatedWebTesting/AutomatedWebTesting/Lib/Features/SDCart.feature
@@ -0,0 +1,64 @@
+﻿Feature: SDCart
+	As a user of the Swap Labs website
+	I want to be able to access the cart
+	In order to edit and checkout my orders
+
+	Background: 
+	Given I am on the inventory page to add items
+	And I add all available items to the cart
+	And I click onto the cart page
+
+@remove
+Scenario Outline: Removing items from the cart
+	When I click on the remove button for item number <itemNum>
+	Then the item <itemName> should be removed from my cart
+	Examples: 
+	| itemNum | itemName  |
+	| 0       | backpack  |
+	| 1       | bikelight |
+	| 2       | boltShirt |
+	| 3       | fleece    |
+	| 4       | onesie    |
+
+@remove @redShirt
+Scenario Outline: Removing red shirt item from the cart
+	When I click on the remove button for the red shirt
+	Then the item <itemName> should be removed from my cart
+	Examples: 
+	| itemName |
+	| redShirt |
+
+@remove
+Scenario Outline: Removing item from the cart decreases the cart count
+	When I click on the remove button for item number <itemNum>
+	Then the cart count should decrease by 1
+	Examples: 
+	| itemNum |
+	| 0       |
+	| 1       |
+	| 2       |
+	| 3       |
+	| 4       |
+
+@remove @redShirt
+Scenario: Removing red shirt item from the cart decreases cart count
+	When I click on the remove button for the red shirt
+	Then the cart count should decrease by 1
+
+	@product
+Scenario Outline: Clicking on the item name link
+	When I click on the name link for the item <itemName>
+	Then I should be taken to the item page with id <itemId>
+	Examples:
+	| itemId | itemName  |
+	| 4      | backpack  |
+	| 0      | bikeLight |
+	| 1      | boltShirt |
+	| 5      | fleece    |
+	| 2      | onesie    |
+	| 3      | redShirt  |
+
+
+
+	# can go back
+	# can checkout
---
+++ AutomatedWebTesting/AutomatedWebTesting/Lib/Steps/SDCartSteps.cs
@@ -0,0 +1,86 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading;
+using TechTalk.SpecFlow;
+
+namespace AutomatedWebTesting.Lib.Features
+{
+    [Binding]
+    public class SDCartSteps
+    {
+        SDWebsite<ChromeDriver> website;
+        int initialCartCount;
+
+        [BeforeScenario]
+        public void SetUp() => website = new SDWebsite<ChromeDriver>();
+
+        [Given(@"I am on the inventory page to add items")]
+        public void GivenIAmOnTheInventoryPageToAddItems()
+        {
+            website.SDCart.GoToInventory();
+        }
+
+
+        [Given(@"I add all available items to the cart")]
+        public void GivenIAddAllAvailableItemsToTheCart()
+        {
+            website.SDCart.AddAllProductsToCart();
+        }
+        
+        [Given(@"I click onto the cart page")]
+        public void GivenIClickOntoTheCartPage()
+        {
+            website.SDCart.GoToCheckout();
+        }
+
+        [When(@"I click on the remove button for item number (.*)")]
+        public void WhenIClickOnTheRemoveButtonForItemNumber(int itemNum)
+        {
+            initialCartCount = website.SDCart.GetCartCount();
+            website.SDCart.RemoveItemFromCart(itemNum);
+        }
+
+        [Then(@"the item (.*) should be removed from my cart")]
+        public void ThenTheItemShouldBeRemovedFromMyCart(string itemName)
+        {
+            Assert.That(website.SDCart.CheckIfItemIsRemovedFromCart(itemName), Is.True);
+        }
+
+        [When(@"I click on the remove button for the red shirt")]
+        public void WhenIClickOnTheRemoveButtonForTheRedShirt()
+        {
+            initialCartCount = website.SDCart.GetCartCount();
+            website.SDCart.RemoveRedShirtFromCart();
+        }
+
+        [Then(@"the cart count should decrease by (.*)")]
+        public void ThenTheCartCountShouldDecreaseBy(int cartDecrease)
+        {
+            Assert.That(website.SDCart.GetCartCount(), Is.EqualTo(initialCartCount - cartDecrease));
+        }
+
+        //website.SDCart.ClickItemNameCartLink(itemName);
+        //    Assert.That(website.SeleniumDriver.Url, Is.EqualTo($"https://www.saucedemo.com/inventory-item.html?id=" + itemId.ToString()));
+
+
+        [When(@"I click on the name link for the item (.*)")]
+        public void WhenIClickOnTheNameLinkForTheItem(string itemName)
+        {
+            website.SDCart.ClickItemNameCartLink(itemName);
+        }
+
+
+        [Then(@"I should be taken to the item page with id (.*)")]
+        public void ThenIShouldBeTakenToTheItemPageWithId(int itemId)
+        {
+            Thread.Sleep(5000);
+            Assert.That(website.SeleniumDriver.Url, Is.EqualTo($"https://www.saucedemo.com/inventory-item.html?id=" + itemId.ToString()));
+        }
+
+
+
+
+
+    }
+}
