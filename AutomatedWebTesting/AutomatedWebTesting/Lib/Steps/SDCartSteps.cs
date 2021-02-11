98751d711f0d5df6dbc5e7b18c9de32cb8968c60
Zair <michaelxzr@gmail.com>
2021-02-11 18:25:15 +0000

    Update SDCartSteps.cs

--- AutomatedWebTesting/AutomatedWebTesting/Lib/Steps/SDCartSteps.cs
+++ AutomatedWebTesting/AutomatedWebTesting/Lib/Steps/SDCartSteps.cs
@@ -1,7 +1,6 @@
-﻿using NUnit.Framework;
+﻿using System.Threading;
+using NUnit.Framework;
 using OpenQA.Selenium.Chrome;
-using System;
-using System.Threading;
 using TechTalk.SpecFlow;
 
 namespace AutomatedWebTesting.Lib.Features
@@ -9,78 +8,67 @@ namespace AutomatedWebTesting.Lib.Features
     [Binding]
     public class SDCartSteps
     {
-        SDWebsite<ChromeDriver> website;
-        int initialCartCount;
+        private int _initialCartCount;
+        private SDWebsite<ChromeDriver> _website;
 
         [BeforeScenario]
-        public void SetUp() => website = new SDWebsite<ChromeDriver>();
+        public void SetUp() => _website = new SDWebsite<ChromeDriver>();
 
         [Given(@"I am on the inventory page to add items")]
         public void GivenIAmOnTheInventoryPageToAddItems()
         {
-            website.SDCart.GoToInventory();
+            _website.SDCart.GoToInventory();
         }
-
 
         [Given(@"I add all available items to the cart")]
         public void GivenIAddAllAvailableItemsToTheCart()
         {
-            website.SDCart.AddAllProductsToCart();
+            _website.SDCart.AddAllProductsToCart();
         }
         
         [Given(@"I click onto the cart page")]
         public void GivenIClickOntoTheCartPage()
         {
-            website.SDCart.GoToCheckout();
+            _website.SDCart.GoToCheckout();
         }
 
         [When(@"I click on the remove button for item number (.*)")]
         public void WhenIClickOnTheRemoveButtonForItemNumber(int itemNum)
         {
-            initialCartCount = website.SDCart.GetCartCount();
-            website.SDCart.RemoveItemFromCart(itemNum);
+            _initialCartCount = _website.SDCart.GetCartCount();
+            _website.SDCart.RemoveItemFromCart(itemNum);
         }
 
         [Then(@"the item (.*) should be removed from my cart")]
         public void ThenTheItemShouldBeRemovedFromMyCart(string itemName)
         {
-            Assert.That(website.SDCart.CheckIfItemIsRemovedFromCart(itemName), Is.True);
+            Assert.That(_website.SDCart.CheckIfItemIsRemovedFromCart(itemName), Is.True);
         }
 
         [When(@"I click on the remove button for the red shirt")]
         public void WhenIClickOnTheRemoveButtonForTheRedShirt()
         {
-            initialCartCount = website.SDCart.GetCartCount();
-            website.SDCart.RemoveRedShirtFromCart();
+            _initialCartCount = _website.SDCart.GetCartCount();
+            _website.SDCart.RemoveRedShirtFromCart();
         }
 
         [Then(@"the cart count should decrease by (.*)")]
         public void ThenTheCartCountShouldDecreaseBy(int cartDecrease)
         {
-            Assert.That(website.SDCart.GetCartCount(), Is.EqualTo(initialCartCount - cartDecrease));
+            Assert.That(_website.SDCart.GetCartCount(), Is.EqualTo(_initialCartCount - cartDecrease));
         }
-
-        //website.SDCart.ClickItemNameCartLink(itemName);
-        //    Assert.That(website.SeleniumDriver.Url, Is.EqualTo($"https://www.saucedemo.com/inventory-item.html?id=" + itemId.ToString()));
-
 
         [When(@"I click on the name link for the item (.*)")]
         public void WhenIClickOnTheNameLinkForTheItem(string itemName)
         {
-            website.SDCart.ClickItemNameCartLink(itemName);
+            _website.SDCart.ClickItemNameCartLink(itemName);
         }
-
 
         [Then(@"I should be taken to the item page with id (.*)")]
         public void ThenIShouldBeTakenToTheItemPageWithId(int itemId)
         {
             Thread.Sleep(5000);
-            Assert.That(website.SeleniumDriver.Url, Is.EqualTo($"https://www.saucedemo.com/inventory-item.html?id=" + itemId.ToString()));
+            Assert.That(_website.SeleniumDriver.Url, Is.EqualTo($"https://www.saucedemo.com/inventory-item.html?id=" + itemId.ToString()));
         }
-
-
-
-
-
     }
 }
